55870311ed591ebdd7489f4acb5cfc51053dcaa5
Btrya <584683599@qq.com>
2022-04-17 00:45:59 +0800

    feature: compiler - 解析 element

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,5 +1,10 @@
 import { NodeTypes } from "./ast"
 
+const enum TagTypes {
+  START,
+  END
+}
+
 export function baseParse(content: string) {
   const context = createParserContext(content)
   return createRoot(parseChildren(context))
@@ -8,11 +13,36 @@ export function baseParse(content: string) {
 function parseChildren(context) {
   const nodes: any = []
   let node
-  if (context.source.startsWith("{{")) {
+  const s = context.source
+  if (s.startsWith("{{")) {
     node = parseInterpolation(context)
+  } else if (s[0] === "<") {
+    if (/[a-z]/i.test(s[1])) {
+      node = parseElement(context)
+    }
   }
   nodes.push(node)
   return nodes
+}
+
+function parseElement(context) {
+  // 1. 解析 tag
+  const element = parseTag(context, TagTypes.START)
+  parseTag(context, TagTypes.END)
+  return element
+}
+
+function parseTag(context: any, type: TagTypes) {
+  const match: any = /^<\/?([a-z]*)/i.exec(context.source)
+  const tag = match[1]
+  // 2. 删除处理完成的代码
+  advanceBy(context, match[0].length)
+  advanceBy(context, 1)
+  if (type === TagTypes.END) return  
+  return {
+    type: NodeTypes.ELEMENT,
+    tag
+  }
 }
 
 function parseInterpolation(context) {
